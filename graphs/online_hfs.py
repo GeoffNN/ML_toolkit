fe36e18807f7ff74c894d358cf0ab504045eca09
GeoffNN <geoffrey.negiar@polytechnique.edu>
2016-12-20 03:50:56 +0100

    Implemented centroid_update

--- graphs/online_hfs.py
+++ graphs/online_hfs.py
@@ -1,18 +1,20 @@
-from graphs.graph_init import build_graph, build_laplacian
+from scipy.spatial.distance import pdist
+
+from graphs.graph_init import build_graph, build_laplacian, GraphParams, LaplacianParams
 import numpy as np
 
 
 class OnlineCoverState:
-    def __init__(self, centroids, nodes_to_centroids_map, centroids_to_nodes_map, R, taboo, max_n_centroids, graph_params, laplacian_params):
-        self.centroids = centroids
-        self.n_centroids = len(centroids)
+    def __init__(self, n_samples, max_n_centroids=20, graph_params=GraphParams(),
+                 laplacian_params=LaplacianParams()):
+        self.centroids = []
+        self.n_centroids = 0
         self.max_n_centroids = max_n_centroids
-        self.nodes_to_centroids_map = nodes_to_centroids_map
-        self.centroids_to_nodes_map = centroids_to_nodes_map
+        self.nodes_to_centroids_map = np.zeros(n_samples)
+        self.centroids_to_nodes_map = np.zeros(max_n_centroids)
         self.graph_params = graph_params
         self.laplacian_params = laplacian_params
-        self.R = R
-        self.taboo = taboo
+        self.R = 0
 
     def online_compute_solution(self, t, Y):
         n_classes = len(Y.unique())
@@ -45,5 +47,54 @@ class OnlineCoverState:
         return new_label
 
     def online_update_centroids(self, t, datum, num_labels):
-        online_state = []
-        return online_state
+        if self.n_centroids == self.max_n_centroids:
+            W = build_graph(self.centroids, self.graph_params)
+            # Transform similarities into distances
+            distances = 1. / (W + 10 ** (-30))
+
+            # Put taboo and self loops to infinity
+            distances[:, self.taboo] = np.inf
+            distances[self.taboo, :] = np.inf
+            np.fill_diagonal(distances, np.inf)
+
+            idx = distances.argmax()
+            c_add, c_rep = np.unravel_index(idx, distances.shape)
+            dx = np.min(1. / (
+                np.exp(-pdist(self.centroids, datum, 'euclidean') / (2 * self.graph_param.sigma2)) + 10 ** (-30)));
+
+            if c_rep not in self.taboo and c_rep > num_labels:
+                if np.sum(self.nodes_to_centroids_map == self.centroids_to_nodes_map[c_add]) > np.sum(
+                                self.nodes_to_centroids_map == self.centroids_to_nodes_map[c_rep]):
+                    auxv = c_rep
+                    c_rep = c_add
+                    c_add = auxv
+
+            if self.R == 0:
+                self.R = distances.min()
+                # taboo is index: careful
+                self.taboo = np.zeros(self.max_n_centroids, dtype=bool)
+                self.taboo[num_labels] = True
+                print("R = {}".format(self.R))
+                print("{} quantization vertices".format(len(self.n_centroids)))
+            elif dx > self.R:
+                R *= 1.5
+                self.taboo = np.zeros(self.max_n_centroids, dtype=bool)
+                self.taboo[num_labels] = True
+                print("R = {}".format(self.R))
+                print("{} quantization vertices".format(len(self.n_centroids)))
+
+            idx = np.nonzero(self.nodes_to_centroids_map == self.centroids_to_nodes_map[c_add])
+            self.nodes_to_centroids_map[idx] = self.centroids_to_nodes_map[c_rep]
+
+            self.taboo[c_rep] = True
+
+            self.centroids_to_nodes[c_add] = t
+            self.centroids += [datum]
+            self.nodes_to_centroids_map[t] = c_add
+
+
+        else:
+            self.centroids_to_nodes_map.append(t)
+            self.nodes_to_centroids_map[t] = t
+            self.centroids.append[datum]
+            self.taboo[t] = False
