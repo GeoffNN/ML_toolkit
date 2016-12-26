7fbb54de820271f85386fdf14ce8487a82c956e9
GeoffNN <geoffrey.negiar@polytechnique.edu>
2016-12-26 02:46:32 +0100

    Implemented iterative_hfs

--- graphs/hard_hfs.py
+++ graphs/hard_hfs.py
@@ -1,16 +1,18 @@
 import numpy as np
 from numpy.linalg import inv
 
-from graphs.generate_data import blobs
 from graphs.graph_init import GraphParams, LaplacianParams, build_graph, build_laplacian
 
 
 # TODO: refactor using fit/transform
 
-def hfs(X, Y, graph_params=GraphParams(), laplacian_params=LaplacianParams(), mode='simple'):
+def hfs(X, Y, graph_params=GraphParams(), laplacian_params=LaplacianParams(), mode='simple', **kwargs):
     if mode is 'simple':
         return simple_hfs(X, Y, graph_params, laplacian_params)
     elif mode is 'iterative':
+        if 't' in kwargs.keys():
+            T = kwargs['t']
+            return iterative_hfs(X, Y, graph_params, laplacian_params, T=T)
         return iterative_hfs(X, Y, graph_params, laplacian_params)
     elif mode is 'online':
         return online_hfs(X, Y, graph_params, laplacian_params)
@@ -42,15 +44,30 @@ def simple_hfs(X, Y, graph_params, laplacian_params):
     return labels + 1
 
 
-def iterative_hfs(X, Y, graph_params, laplacian_params):
+def iterative_hfs(X, Y, graph_params, laplacian_params, T=50):
     W = build_graph(X, graph_params)
     L = build_laplacian(W, laplacian_params)
 
-    pass
+    n_samples = len(X)
+    n_classes = len(np.unique(Y)) - 1
+
+    # compute linear target for labelled samples
+    l_idx = np.nonzero(Y)[0]
+    u_idx = np.nonzero(Y == 0)[0]
+    n_l = len(l_idx)
+    n_u = len(u_idx)
+    y = -np.ones((n_samples, n_classes))
+    for i in range(n_l):
+        y[int(l_idx[i]), int(Y[l_idx[i]] - 1)] = 1
+
+    D = W.sum(axis=0)
+    f = y
+    for t in range(T):
+        for k in u_idx:
+            f[k, :] = W[:, k].dot(f) / D[k]
+    labels = f.argmax(axis=1) + 1
+    return labels
 
 
 def online_hfs(X, Y, graph_params, laplacian_params):
     pass
-
-
-
